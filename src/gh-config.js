fa2965132d897d05f1706ca4d224752762f32e5f
Chris McMaster <mcmastc1@gmail.com>
2023-12-05 09:37:52 +1100

    Updated model list

--- src/gh-config.js
+++ src/gh-config.js
@@ -1,19 +1,17 @@
 export default {
 	"model_list": [
 		{
-			"model_url": "https://huggingface.co/mlc-ai/mlc-chat-RedPajama-INCITE-Chat-3B-v1-q4f16_1/resolve/main/",
-			"local_id": "RedPajama-INCITE-Chat-3B-v1-q4f16_1",
-			"required_features": ["shader-f16"],
+			"model_url": "https://huggingface.co/mlc-ai/mlc-chat-RedPajama-INCITE-Chat-3B-v1-q4f32_1/resolve/main/",
+			"local_id": "RedPajama-INCITE-Chat-3B-v1-q4f32_1",
 		},
 		{
-			"model_url": "https://huggingface.co/mlc-ai/mlc-chat-Mistral-7B-Instruct-v0.1-q4f16_1/resolve/main/",
-			"local_id": "Mistral-7B-Instruct-v0.1-q4f16_1",
-			"required_features": ["shader-f16"],
+			"model_url": "https://huggingface.co/mlc-ai/mlc-chat-Mistral-7B-Instruct-v0.1-q4f32_1/resolve/main/",
+			"local_id": "Mistral-7B-Instruct-v0.1-q4f32_1",
 		}
 	],
 	"model_lib_map": {
-		"RedPajama-INCITE-Chat-3B-v1-q4f16_1": "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/RedPajama-INCITE-Chat-3B-v1-q4f16_1-webgpu.wasm",
-		"Mistral-7B-Instruct-v0.1-q4f16_1": "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/Mistral-7B-Instruct-v0.1-q4f16_1-sw4096_cs1024-webgpu.wasm"
+		"RedPajama-INCITE-Chat-3B-v1-q4f32_1": "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/RedPajama-INCITE-Chat-3B-v1-q4f32_1-webgpu.wasm",
+		"Mistral-7B-Instruct-v0.1-q4f32_1": "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/Mistral-7B-Instruct-v0.1-q4f32_1-sw4096_cs1024-webgpu.wasm"
 	},
 	"use_web_worker": true
 }
